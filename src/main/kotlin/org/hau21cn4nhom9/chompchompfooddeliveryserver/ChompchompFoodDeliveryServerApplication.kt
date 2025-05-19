09ea984817481b1978a09cf2ef8b5dc171663283
lehuong240823 <lehuong240823@gmail.com>
2025-05-19 22:45:31 +0700

    Update ChompchompFoodDeliveryServerApplication.kt

--- src/main/kotlin/org/hau21cn4nhom9/chompchompfooddeliveryserver/ChompchompFoodDeliveryServerApplication.kt
+++ src/main/kotlin/org/hau21cn4nhom9/chompchompfooddeliveryserver/ChompchompFoodDeliveryServerApplication.kt
@@ -1,11 +1,28 @@
 package org.hau21cn4nhom9.chompchompfooddeliveryserver
 
+import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class ChompchompFoodDeliveryServerApplication
 
 fun main(args: Array<String>) {
     runApplication<ChompchompFoodDeliveryServerApplication>(*args)
+}
+
+@Component
+class ServerInfoLogger {
+    @Value("\${server.port}")
+    private lateinit var port: String
+
+    @Value("\${server.address}")
+    private lateinit var address: String
+
+    @PostConstruct
+    fun logServerInfo() {
+        println("Application is running on address: $address and port: $port")
+    }
 }
